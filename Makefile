cf9ee1ecbadc6279e2c928996b15a62ad7caa726
Declan Gaylo <dgaylo@mit.edu>
2026-08-29 12:39:28 -0400

    add github release action

--- Makefile
+++ Makefile
@@ -4,20 +4,19 @@ TOOLS_DIR ?= $(CURDIR)/tools
 
 ADD_ON_ID ?= dark-theme@dgaylo.com
 
-# Get version number from manifest
-VERSION := $(shell $(TOOLS_DIR)/getVersion.py $(SRC_DIR)/manifest.json)
+SRC_FILES := $(shell find $(SRC_DIR) -type f)
 
-.PHONY: package upload
+.PHONY: package clean
 
-package: $(BUILD_DIR)/package.zip
+package: $(BUILD_DIR)/package.xpi
 
 # Upload to Mozilla for signing
-upload: $(BUILD_DIR)/package.zip
-	python $(TOOLS_DIR)/upload.py $(ADD_ON_ID) $<
+$(BUILD_DIR)/%.xpi: $(BUILD_DIR)/%.zip
+	python $(TOOLS_DIR)/upload.py $(ADD_ON_ID) $< $@
 
 # Create a tarball of the package
-$(BUILD_DIR)/package.zip: FORCE $(BUILD_DIR)
-	cd $(SRC_DIR) && zip -FS $@ *
+$(BUILD_DIR)/%.zip: $(SRC_FILES) | $(BUILD_DIR)
+	cd $(SRC_DIR) && zip -r $@ *
 
 # create the build directory
 $(BUILD_DIR):
@@ -25,5 +24,3 @@ $(BUILD_DIR):
 
 clean:
 	-$(RM) -r $(BUILD_DIR)
-
-FORCE:
--- tools/upload.py
+++ tools/upload.py
@@ -7,109 +7,184 @@ import jwt
 import json
 
 BASE_URL = 'https://addons.mozilla.org/api/v5/addons/'
-TOKEN_EXPIRATION_TIME = 5  # minutes
+POLL_PERIOD = 10 # seconds
+TIME_OUT = 600 # 10 minutes, per Mozilla's own guidance
+
+class Token:
+    EXPIRATION_TIME = 5 * 60  # second
+
+    def __init__(self, issuer, secret):
+        self._issuer = issuer
+        self._secret = secret
+        self._value = self._create()
+
+    def _create(self) -> str:
+        issued_at_time = int(time.time())
+        self._expiration_time = issued_at_time + self.EXPIRATION_TIME
+
+        return jwt.encode(
+            payload={
+                "iss": self._issuer,
+                "jti": secrets.token_hex(),
+                "iat": issued_at_time,
+                "exp": self._expiration_time
+            },
+            key=self._secret,
+            algorithm="HS256"
+        )
+
+    @property
+    def header(self) -> dict[str, str]:
+        if int(time.time()) > self._expiration_time:
+            self._value = self._create()
+
+        return {"Authorization": "JWT "+self._value}
 
 
-def generate_token(issuer, secret):
-    """Get JWT Token"""
+class Upload:
+    def __init__(self, file_path, token: Token):
+        self._token = token
 
-    issued_at_time = int(time.time())
-    expiration_time = issued_at_time + 60 * TOKEN_EXPIRATION_TIME
-    jwt_id = secrets.token_hex()
+        # upload the file
+        with open(file_path, "rb") as f:
+            response = requests.post(
+                BASE_URL+"upload/",
+                headers=self._token.header,
+                files={"upload": f},
+                data={"channel": "unlisted"},
+                timeout=10,
+                )
 
-    payload = {
-        "iss": issuer,
-        "jti": jwt_id,
-        "iat": issued_at_time,
-        "exp": expiration_time
-    }
+        # check response status
+        if not response.ok:
+            print(f"upload_create failed with {response.status_code}")
+            print(response.text)
+            sys.exit(response.status_code)
 
-    return jwt.encode(payload, secret, algorithm="HS256")
+        # save UUID
+        self.uuid = json.loads(response.text)['uuid']
+        self._final_details = None
+
+    def _get_details(self):
+        response = requests.get(BASE_URL+"upload/"+self.uuid,
+                                headers=self._token.header,
+                                timeout=10)
+
+        # check response status
+        if not response.ok:
+            print(f"upload_detail failed with {response.status_code}")
+            print(response.text)
+            sys.exit(response.status_code)
+
+        return json.loads(response.text)
+
+    @property
+    def details(self):
+        if self._final_details is None:
+            # Wait for processing
+            print("Waiting for processing", end=" ", flush=True)
+            deadline = time.time() + TIME_OUT  
+            while not (details := self._get_details())['processed']:
+                if time.time() > deadline:
+                    print("Timed out waiting for processing", file=sys.stderr)
+                    sys.exit(1)
+                print(".", end="", flush=True)
+                time.sleep(POLL_PERIOD)
+            print(" done")
+
+            self._final_details = details
+
+        return self._final_details
 
 
-def upload_create(file_path, headers):
-    # upload the file
-    with open(file_path, "rb") as f:
-        response = requests.post(BASE_URL+"upload/",
-                                 headers=headers,
-                                 files={"upload": f},
-                                 data={"channel": "unlisted"},
-                                 timeout=10)
+class Version:
+    def __init__(self, upload: Upload, add_on_id, token: Token):
+        self.add_on_id = add_on_id
+        self._token = token
 
-    # check response status
-    if not response.ok:
-        print(f"upload_create failed with {response.status_code}")
-        print(response.text)
-        sys.exit(response.status_code)
+        # create the version
+        response = requests.post(
+            BASE_URL+"addon/"+self.add_on_id+"/versions/",
+            headers=self._token.header,
+            json={"upload": upload.uuid},
+            timeout=10
+        )
 
-    # return uuid
-    return json.loads(response.text)['uuid']
+        # check response status
+        if not response.ok:
+            print(f"version_create failed with {response.status_code}")
+            print(response.text)
+            sys.exit(response.status_code)
 
+        # save version number
+        self.version_number = json.loads(response.text)["version"]
+        self._file_info = None
 
-def upload_detail(uuid, headers):
-    response = requests.post(BASE_URL+"upload/"+uuid,
-                             headers=headers,
-                             timeout=10)
+    def _get_details(self):
+        response = requests.get(
+            BASE_URL + "addon/" + self.add_on_id + "/versions/" + self.version_number + "/",
+            headers=self._token.header,
+            timeout=10
+        )
 
-    # check response status
-    if not response.ok:
-        print(f"upload_detail failed with {response.status_code}")
-        print(response.text)
-        sys.exit(response.status_code)
+        if not response.ok:
+            print(f"version_detail failed with {response.status_code}")
+            print(response.text)
+            sys.exit(response.status_code)
 
-    return json.loads(response.text)
+        return json.loads(response.text)
 
+    @property
+    def file_info(self):
+        if self._file_info is None:
+            # Wait for signing
+            print("Waiting for signing", end=" ", flush=True)
+            deadline = time.time() + TIME_OUT
+            while not (file_info := self._get_details().get("file", {})).get("status") == "public":
+                if time.time() > deadline:
+                    print("Timed out waiting for signing", file=sys.stderr)
+                    sys.exit(1)
+                print(".", end="", flush=True)
+                time.sleep(POLL_PERIOD)
+            print(" done")
 
-def version_create(uuid, add_on_id, headers):
-    response = requests.post(BASE_URL+"addon/"+add_on_id+"/versions/",
-                             headers=headers,
-                             json={"upload": uuid},
-                             timeout=10)
+            self._file_info = file_info
 
-    # check response status
-    if not response.ok:
-        print(f"version_create failed with {response.status_code}")
-        print(response.text)
-        sys.exit(response.status_code)
-
-    return json.loads(response.text)
+        return self._file_info
 
 
 add_on_id = sys.argv[1]
 file_path = sys.argv[2]
+output_path = sys.argv[3]
 
-headers = {
-    "Authorization": "JWT "+generate_token(
-        issuer=os.environ['JWT_ISSUER'],
-        secret=os.environ['JWT_SECRET'])
-}
+token = Token(
+    issuer=os.environ['JWT_ISSUER'],
+    secret=os.environ['JWT_SECRET']
+    )
 
 
 # Upload
-upload = upload_create(file_path, headers)
-print("Uploaded "+upload)
+upload = Upload(file_path, token)
+print("Uploaded " + upload.uuid)
 
-# Wait for processing
-print("Waiting for processing", end=" ", flush=True)
-time.sleep(5)
-detail = upload_detail(upload, headers)
-
-while not detail['processed']:
-    print(".", end="", flush=True)
-    time.sleep(5)
-    detail = upload_detail(upload, headers)
-
-print(" done")
-
-if not detail['valid']:
+# Validate upload
+if not upload.details['valid']:
     print("Upload not valid, see upload_details.json", file=sys.stderr)
 
     with open("upload_details.json", "w", encoding="utf-8") as f:
-        json.dump(detail, f, indent=4)
+        json.dump(upload.details, f, indent=4)
 
     sys.exit(1)
 
 # Create version
-version = version_create(upload, add_on_id, headers)
+version = Version(upload, add_on_id, token)
+print("Created version " + version.version_number)
+download_url = version.file_info["url"]
+print(download_url)
 
-print(json.dumps(version, indent=2))
+# Downloading
+with requests.get(download_url, headers=token.header, stream=True, timeout=30) as r:
+    r.raise_for_status()
+    with open(output_path, "wb") as f:
+        for chunk in r.iter_content(chunk_size=8192):
+            f.write(chunk)
